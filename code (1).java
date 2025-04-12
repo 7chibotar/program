4549ac8c023fbd05e6f0418e00ef2f4590f95301
7chibotar <chibotarrv@college.omsu.ru>
2025-04-12 15:03:51 +0600

    Add files via upload

--- code (1).java
+++ code (1).java
@@ -1,30 +1,15 @@
-// Чиботарь Руслан Владимирович ДТН-309 задание 12
-
+// Чиботарь Руслан Владимирович ДТН-309 задание 1
 import java.util.Scanner;
-public class main11 {
-    public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Введите число: ");
-                int number = scanner.nextInt();
-                if (isPalindrome(number)) {
-                    System.out.println(number + " является палиндромом.");
-                } else {
-                    System.out.println(number + " не является палиндромом.");
-                }
+public class MyClass {
+  public static void main(String args[]) {
 
-                scanner.close();
-            }
-            public static boolean isPalindrome(int number) {
-                int originalNumber = number;
-                int reversedNumber = 0;
-
-                while (number != 0) {
-                    int digit = number % 10;
-                    reversedNumber = reversedNumber * 10 + digit;
-                    number = number / 10;
-                }
-
-
-                return originalNumber == reversedNumber;
-            }
-        }
+   
+    Scanner scan = new Scanner(System.in);
+    System.out.print ("Ведите число:");   
+    String num = scan.nextLine();
+    System.out.println("Ваше число:" + num);
+   
+    
+    
+  }
+}
---
+++ code (2).java
@@ -0,0 +1,17 @@
+// Чиботарь Руслан Владимирович ДТН-309 задание 2
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print ("Введите  число:");
+        int num1 = scan.nextInt ();
+        System.out.println("Введите  число:");
+        int num2 = scan.nextInt ();
+
+        int res;
+        res = num1 + num2;
+        System.out.print("результат: " + res);}
+    }
+
